619ecd551d5d9f9f41a0227ec49d6608695d60c0
moinulislam <moinul.islam@enosisbd.com>
2022-01-10 03:04:42 +0600

    fourth test added

--- Cypress_Automation/Project1/cypress/integration/test_1.js
+++ Cypress_Automation/Project1/cypress/integration/test_1.js
@@ -10,26 +10,43 @@ it('find element and click', function(){
 });
 
 it('saving value of an element and assert that', function(){
-    cy.visit("https://moinshawon.github.io/")
+    cy.visit("https://moinshawon.github.io/")                               // default assertion: get req 200
     cy.url().should('include', 'moinshawon')                                // asserting url has that text
-    cy.get('h1').should('include.text', 'MOINUL ISLAM')                     // accerting h1 has that text
+    cy.get('h1').should('include.text', 'MOINUL ISLAM')                     // accerting h1 has that text // implicit assertion
     cy.get('#part-time > :nth-child(1) > :nth-child(2) > a').should($h => { // saving the text of that element in $h parameter 
         console.log($h.text())                                              // print in the console
-        expect($h.text()).to.eq(" Enosis Solutions")                        // assert that $h has valid text
+        expect($h.text()).to.eq(" Enosis Solutions")                        // assert that $h has valid text // explicit assertion
     })
     cy.get('#part-time > :nth-child(1) > :nth-child(2) > a').should('have.text', " Enosis Solutions") // doing prev work differently
-    cy.get('#part-time').find('a').first().should('have.text', " Enosis Solutions") // the css selector is bad practice so, i tried to do this
+    cy.get('#part-time').find('a').first().should('have.text', " Enosis Solutions") // the css selector is a bad practice so, i tried to do this
 })
 
-it.only('saving value of an element and using it for future work', function(){
-    cy.visit("https://mehruzsaif.github.io/Mission-2022/")
-    cy.get('.fakibaj').then($lblValue => {
-        console.log($lblValue)
-        cy.wrap($lblValue).as('fakibazWord')                                // saving the value of that element in fakibazWord variable
+it('saving value of an element and using it for future work', function(){
+    cy.visit("https://moinshawon.github.io/")
+    cy.get('section.bottom-bar > h5').then($lblValue => {
+        cy.wrap($lblValue).as('copyrightWord')                                // wrap makes the word an element
     })
-    cy.get('@fakibazWord').then(a => {                                      // need to use function bcz cypress execute normal code first then function
-        console.log(a.text())                                               // will print the value that was saved in the variable
+    cy.get('@copyrightWord').then(a => {                                      // need to use function bcz cypress execute normal code first then function
+        console.log(a.text())                                                 // will print the value that was saved in the variable
     })
 })
 
+it('working on multiple elements in cypress', function(){
+    const listOfNavItem = [
+        "Model S",
+        "Model X",
+        "Model 3",
+        "Roadster",
+        "Want to go Mars?"
+    ]
 
+    cy.visit("https://moinshawon.ml/tesla-bootstrap/")
+    cy.get(".mx-auto").find('li').should('have.length', 5)
+    cy.get(".mx-auto").find('li').each((item, index, list) => {               // element, num of list, whole list loop thought 5 times
+        // console.log(list)                                                  // print the whole list 5 times as it will loop through
+        console.log(item.text())                                              // print single item each in loop
+        cy.wrap(item).should('contain.text', listOfNavItem[index])            // assert element contains text 'Model S'
+        console.log(index)                                                    // print the idx of the list
+        expect(Cypress.$(item).text()).to.eq(listOfNavItem[index])            // assert element's text to eq 'Model S'
+    })
+})
